fe4e9bd796875b8d0b280461d03afa202e6f19a9
antonio <antonioguil1988@gmail.com>
2024-08-30 11:00:31 +0200

    separación de funciones en builin_export para que cumplan 25 líneas

--- src/builtins/builtin_export.c
+++ src/builtins/builtin_export.c
@@ -34,343 +34,67 @@ char	**replace_envp(char **old_envp, char **new_envp)
 	return (new_envp);
 }
 
-/**
- * Processes a variable string and updates the environment if necessary.
- * Checks if the variable string is valid, removes quotes if necessary,
- * and updates the environment if the variable does not already exist.
- * @param str The variable string to process.
- * @param env The environment structure to update.
- */
-/*
-static void	process_and_update_var(char *str, t_env *env)
-{
-	int		eq_idx;
-	char	**new_envp;
 
-	eq_idx = equal_sign(str);
-	if (!is_valid_environment_variable(str))
-		return ;
-	if (eq_idx != -1 && str[eq_idx + 1] == '\"')
-		delete_quotes(&str[eq_idx + 1], '\"');
-	if (is_valid_identifier(str))
-	{
-		if (!variable_exist(env, str))
-		{
-			new_envp = add_var(env->envp_cpy, str);
-			if (new_envp == NULL)
-			{
-				perror("Error al agregar variable de entorno");
-				return ;
-			}
-			env->envp_cpy = replace_envp(env->envp_cpy, new_envp);
-		}
-	}
-	else
-		handle_export_errors(str);
+static void ft_update_var(char *str, t_env *env, int eq_idx)
+{
+    char **new_envp;
+    if (eq_idx == -1)
+        update_export_list(env, str);
+    else
+    {
+        if (!variable_exist(env, str))
+        {
+            new_envp = add_var(env->envp_cpy, str);
+            if (new_envp == NULL)
+            {
+                perror("Error al agregar variable de entorno");
+                return ;
+            }
+            env->envp_cpy = replace_envp(env->envp_cpy, new_envp);
+        }
+        update_export_list(env, str);
+    }
 }
-*/
+
 
 static void process_and_update_var(char *str, t_env *env)
 {
     int eq_idx;
-    char **new_envp;
 
     eq_idx = equal_sign(str);
     if (!is_valid_environment_variable(str))
         return ;
-
     if (eq_idx != -1 && str[eq_idx + 1] == '\"')
         delete_quotes(&str[eq_idx + 1], '\"');
-
     if (is_valid_identifier(str))
-    {
-        if (eq_idx == -1) // Variable sin valor
-        {
-            update_export_list(env, str);
-        }
-        else
-        {
-            if (!variable_exist(env, str))
-            {
-                new_envp = add_var(env->envp_cpy, str);
-                if (new_envp == NULL)
-                {
-                    perror("Error al agregar variable de entorno");
-                    return;
-                }
-                env->envp_cpy = replace_envp(env->envp_cpy, new_envp);
-            }
-            // Actualizar la lista de exportación también
-            update_export_list(env, str);
-        }
-    }
+        ft_update_var(str, env, eq_idx);
     else
-    {
         handle_export_errors(str);
-    }
 }
 
-
-/**
- * Updates the environment with variables specified in the arguments.
- * Iterates over the arguments, starting from the second argument, and
- * processes each one to update the environment.
- * @param args The arguments containing variable strings.
- * @param env The environment structure to update.
- */
-
-void	update_environment(char **args, t_env *env)
-{
-	int	i;
-
-	i = 1;
-	while (args[i] != NULL)
-	{
-		process_and_update_var(args[i], env);
-		i++;
-	}
-}
-
-/**
- * Implements the 'export' command to update environment variables.
- * If no additional arguments are provided, it lists the current environment.
- * Otherwise, it updates the environment with the provided variable strings.
- * @param args The arguments passed to the 'export' command.
- * @param env The environment structure to modify.
- */
-/*
-void	builtin_export(char **args, t_env *env)
-{
-	if (args[1] == NULL)
-	{
-		builtin_env(args, env);
-		return ;
-	}
-	update_environment(args, env);
-}
-*/
-/*
 void update_export_list(t_env *env, char *str)
 {
     char **new_export_cpy;
-    int size;
-    int i;
 
     if (!env->export_cpy)
     {
-        env->export_cpy = malloc(sizeof(char *));
-        if (!env->export_cpy)
-        {
-            perror("Error al asignar memoria");
-            exit(EXIT_FAILURE);
-        }
-        env->export_cpy[0] = NULL;
-    }
-
-    size = 0;
-    while (env->export_cpy[size])
-        size++;
-
-    new_export_cpy = malloc((size + 2) * sizeof(char *));
-    if (!new_export_cpy)
-    {
-        perror("Error al asignar memoria");
-        exit(EXIT_FAILURE);
-    }
-
-    i = 0;
-    while (i < size && strcmp(env->export_cpy[i], str) < 0)
-    {
-        new_export_cpy[i] = strdup(env->export_cpy[i]);
-        if (!new_export_cpy[i])
-        {
-            perror("Error al duplicar cadena");
-            exit(EXIT_FAILURE);
-        }
-        i++;
-    }
-    new_export_cpy[i] = strdup(str);
-    if (!new_export_cpy[i])
-    {
-        perror("Error al duplicar cadena");
-        exit(EXIT_FAILURE);
-    }
-    i++;
-    while (i <= size)
-    {
-        new_export_cpy[i] = strdup(env->export_cpy[i - 1]);
-        if (!new_export_cpy[i])
-        {
-            perror("Error al duplicar cadena");
-            exit(EXIT_FAILURE);
-        }
-        i++;
-    }
-    new_export_cpy[size + 1] = NULL;
-
-    free_arr(env->export_cpy, size);
-    env->export_cpy = new_export_cpy;
-}
-*/
-/*
-void update_export_list(t_env *env, char *str)
-{
-    char **new_export_cpy;
-    int size;
-    int i;
-
-    // Verificar si export_cpy es NULL y manejar adecuadamente
-    if (!env->export_cpy)
-    {
-        new_export_cpy = malloc(sizeof(char *) * 2);
+        new_export_cpy = add_var(NULL, str);
         if (!new_export_cpy)
         {
-            perror("Error al asignar memoria");
+            perror("Error al agregar variable de entorno");
             exit(EXIT_FAILURE);
         }
-        new_export_cpy[0] = strdup(str);
-        if (!new_export_cpy[0])
-        {
-            perror("Error al duplicar cadena");
-            exit(EXIT_FAILURE);
-        }
-        new_export_cpy[1] = NULL;
         env->export_cpy = new_export_cpy;
         return;
     }
 
-    // Contar el tamaño actual del array
-    size = 0;
-    while (env->export_cpy[size])
-        size++;
-
-    // Reservar memoria para el nuevo array
-    new_export_cpy = malloc(sizeof(char *) * (size + 2));
+    new_export_cpy = add_var(env->export_cpy, str);
     if (!new_export_cpy)
     {
-        perror("Error al asignar memoria");
+        perror("Error al agregar variable de entorno");
         exit(EXIT_FAILURE);
     }
-
-    // Copiar elementos existentes y añadir la nueva variable
-    i = 0;
-    while (i < size && strcmp(env->export_cpy[i], str) < 0)
-    {
-        new_export_cpy[i] = strdup(env->export_cpy[i]);
-        if (!new_export_cpy[i])
-        {
-            perror("Error al duplicar cadena");
-            free_arr(new_export_cpy, i);
-            exit(EXIT_FAILURE);
-        }
-        i++;
-    }
-
-    new_export_cpy[i] = strdup(str);
-    if (!new_export_cpy[i])
-    {
-        perror("Error al duplicar cadena");
-        free_arr(new_export_cpy, i);
-        exit(EXIT_FAILURE);
-    }
-    i++;
-
-    while (i <= size)
-    {
-        new_export_cpy[i] = strdup(env->export_cpy[i - 1]);
-        if (!new_export_cpy[i])
-        {
-            perror("Error al duplicar cadena");
-            free_arr(new_export_cpy, i);
-            exit(EXIT_FAILURE);
-        }
-        i++;
-    }
-    new_export_cpy[size + 1] = NULL;
-
-    // Liberar memoria del array anterior
-    free_arr(env->export_cpy, size);
-    env->export_cpy = new_export_cpy;
-}
-*/
-void update_export_list(t_env *env, char *str)
-{
-    char **new_export_cpy;
-    int size;
-    int i;
-
-    // Verificar si export_cpy es NULL y manejar adecuadamente
-    if (!env->export_cpy)
-    {
-        new_export_cpy = malloc(sizeof(char *) * 2);
-        if (!new_export_cpy)
-        {
-            perror("Error al asignar memoria");
-            exit(EXIT_FAILURE);
-        }
-        new_export_cpy[0] = strdup(str);
-        if (!new_export_cpy[0])
-        {
-            perror("Error al duplicar cadena");
-            exit(EXIT_FAILURE);
-        }
-        new_export_cpy[1] = NULL;
-        env->export_cpy = new_export_cpy;
-        return;
-    }
-
-    // Contar el tamaño actual del array
-    size = 0;
-    while (env->export_cpy[size])
-        size++;
-
-    // Reservar memoria para el nuevo array
-    new_export_cpy = malloc(sizeof(char *) * (size + 2));
-    if (!new_export_cpy)
-    {
-        perror("Error al asignar memoria");
-        exit(EXIT_FAILURE);
-    }
-
-    // Copiar elementos existentes y añadir la nueva variable
-    i = 0;
-    while (i < size && strcmp(env->export_cpy[i], str) < 0)
-    {
-        new_export_cpy[i] = strdup(env->export_cpy[i]);
-        if (!new_export_cpy[i])
-        {
-            perror("Error al duplicar cadena");
-            free_arr(new_export_cpy, i);
-            exit(EXIT_FAILURE);
-        }
-        i++;
-    }
-
-    new_export_cpy[i] = strdup(str);
-    if (!new_export_cpy[i])
-    {
-        perror("Error al duplicar cadena");
-        free_arr(new_export_cpy, i);
-        exit(EXIT_FAILURE);
-    }
-    i++;
-
-    while (i <= size)
-    {
-        new_export_cpy[i] = strdup(env->export_cpy[i - 1]);
-        if (!new_export_cpy[i])
-        {
-            perror("Error al duplicar cadena");
-            free_arr(new_export_cpy, i);
-            exit(EXIT_FAILURE);
-        }
-        i++;
-    }
-    new_export_cpy[size + 1] = NULL;
-
-    // Liberar memoria del array anterior
-    free_arr(env->export_cpy, size);
-    env->export_cpy = new_export_cpy;
+    env->export_cpy = replace_envp(env->export_cpy, new_export_cpy);
 }
 
 void print_export_list(t_env *env)
@@ -380,13 +104,13 @@ void print_export_list(t_env *env)
     if (env->export_cpy == NULL)
     {
         printf("export_cpy is NULL\n");
-        return;
+        return ;
     }
-
     i = 0;
     while (env->export_cpy[i])
     {
-        printf("declare -x %s\n", env->export_cpy[i]);
+        if (strcmp(env->export_cpy[i], "export") != 0)
+            printf("declare -x %s\n", env->export_cpy[i]);
         i++;
     }
 }
@@ -396,15 +120,15 @@ void builtin_export(char **args, t_env *env)
 {
     int i;
 
-    if (!args[1]) // Si no hay argumentos, imprimir todas las variables exportadas
-    {
+    i = 0;
+    if (!args[1])
         print_export_list(env);
-    }
     else
     {
-        for (i = 1; args[i]; i++)
+        while (args[i])
         {
             process_and_update_var(args[i], env);
+            i++;
         }
     }
 }
