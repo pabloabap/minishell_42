2c313f47936437c27dbcaaf4b51210b70c45e134
pabad-ap_msi <pablo.abad96@gmail.com>
2024-09-02 21:32:33 +0200

    Creacion de ficheros cuando solo redirecciones y eliminacion de un gestor de error innecesario

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -117,8 +117,7 @@ int	ft_lex_to_cmd(t_lexem **lex_list, t_single_cmd **cmd, int *err_n)
 	t_lexem	*cmds_start;
 
 	cmds_start = *lex_list;
-	if (grammar_checks(*lex_list) == EXIT_SUCCESS && \
-	ft_red_err(*lex_list) == EXIT_SUCCESS)
+	if (grammar_checks(*lex_list) == EXIT_SUCCESS)
 	{
 		while (cmds_start && cmds_start->token >= IN_REDIR \
 		&& cmds_start->next->token <= DOUBLE_QUOTES)
