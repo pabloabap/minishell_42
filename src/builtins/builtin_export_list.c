02036ed0bd1a001410c4914ba3aa755b8c87c4ea
antonio <antonioguil1988@gmail.com>
2024-09-03 19:11:54 +0200

    Algunos cambios de norminette

--- src/builtins/builtin_export_list.c
+++ src/builtins/builtin_export_list.c
@@ -48,6 +48,7 @@ void	update_export_list(t_env *env, char *str)
 	env->export_cpy = replace_envp(env->export_cpy, new_export_cpy);
 }
 */
+/*
 void update_export_list(t_env *env, char *str)
 {
     char **new_export_cpy;
@@ -117,7 +118,70 @@ void update_export_list(t_env *env, char *str)
     }
     env->export_cpy = replace_envp(env->export_cpy, new_export_cpy);
 }
+*/
+void update_export_list(t_env *env, char *str)
+{
+    char    **new_export_cpy;
+    int     i;
+    char    *equal_sign;
+    char    *name;
+    size_t  name_len;
 
+    if (!env->export_cpy)
+    {
+        new_export_cpy = add_var(NULL, str);
+        if (!new_export_cpy)
+        {
+            perror("Failure adding environmental variable");
+            exit(EXIT_FAILURE);
+        }
+        env->export_cpy = new_export_cpy;
+        return ;
+    }
+    equal_sign = strchr(str, '=');
+    if (equal_sign != NULL)
+    {
+        name_len = equal_sign - str;
+        name = strndup(str, name_len);
+        if (!name)
+        {
+            perror("Failure duplicating string");
+            exit(EXIT_FAILURE);
+        }
+        for (i = 0; env->export_cpy[i] != NULL; i++)
+        {
+            if (strncmp(env->export_cpy[i], name, name_len) == 0 && 
+                (env->export_cpy[i][name_len] == '=' || env->export_cpy[i][name_len] == '\0'))
+            {
+                free(env->export_cpy[i]);
+                env->export_cpy[i] = strdup(str);
+                if (!env->export_cpy[i])
+                {
+                    perror("Failure updating environmental variable");
+                    exit(EXIT_FAILURE);
+                }
+                free(name);
+                return ;
+            }
+        }
+        free(name);
+    }
+    else
+    {
+        for (i = 0; env->export_cpy[i] != NULL; i++)
+        {
+            if (strcmp(env->export_cpy[i], str) == 0)
+                return ;
+        }
+    }
+    new_export_cpy = add_var(env->export_cpy, str);
+    if (!new_export_cpy)
+    {
+        perror("Failure adding environmental variable");
+        exit(EXIT_FAILURE);
+    }
+    env->export_cpy = replace_envp(env->export_cpy, new_export_cpy);
+}
 
 /**
  * Imprime la lista de variables de entorno exportadas.
@@ -188,6 +252,7 @@ void print_export_list(t_env *env)
     }
 }
 */
+/*
 void print_export_list(t_env *env)
 {
     int i;
@@ -253,6 +318,68 @@ void print_export_list(t_env *env)
                 // Si no hay '=', solo imprimir el nombre
                 printf("declare -x %s\n", entry);
             }
+        }
+        i++;
+    }
+}
+*/
+void print_export_list(t_env *env)
+{
+    int     i;
+    char    *entry;
+    char    *equal_sign;
+    char    *name;
+    char    *value;
+
+    if (env->export_cpy == NULL)
+    {
+        printf("export_cpy is NULL\n");
+        return ;
+    }
+
+    i = 0;
+    while (env->export_cpy[i])
+    {
+        if (ft_strncmp(env->export_cpy[i], "export", 6) != 0)
+        {
+            entry = env->export_cpy[i];
+            equal_sign = ft_strchr(entry, '=');
+
+            if (equal_sign != NULL)
+            {
+                *equal_sign = '\0';
+                name = entry;
+                value = equal_sign + 1;
+                if (value[0] == '"' && value[strlen(value) - 1] == '"')
+                {
+                    char    *escaped_value;
+                    char    *src;
+                    char    *dst;
+
+                    escaped_value = malloc(strlen(value) * 2 + 1);
+                    if (!escaped_value)
+                    {
+                        perror("Failure allocating memory");
+                        exit(EXIT_FAILURE);
+                    }
+                    src = value;
+                    dst = escaped_value;
+                    while (*src)
+                    {
+                        if (*src == '"')
+                            *dst++ = '\\';
+                        *dst++ = *src++;
+                    }
+                    *dst = '\0';
+                    printf("declare -x %s=\"%s\"\n", name, escaped_value);
+                    free(escaped_value);
+                }
+                else
+                    printf("declare -x %s=\"%s\"\n", name, value);
+                *equal_sign = '=';
+            }
+            else
+                printf("declare -x %s\n", entry);
         }
         i++;
     }
